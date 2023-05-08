154aef4752fd9a9ee1bf788e20a18247ff0a1f89
Chetankumarbudihal <105309035+Chetankumarbudihal@users.noreply.github.com>
2023-05-08 22:33:03 +0530

    array.c

--- file.c
+++ file.c
@@ -1,17 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<time.h>
 
- main()
+void store_to_arry(int n,int arr1[100]);
+void store(int n);
+void sort(int n,int arr1[100]);
+
+
+main()
 {
-    FILE*ptr=NULL;
-    ptr=fopen("myfile1.txt","w");
-    char a[20];
-    gets(a);
-    fprintf(ptr,"%s",a);
-    puts(a);
-
-    return 0;
-
-
+int arr1[100],n;
+store(n);
+store_to_arry(n,arr1);
+sort(n,arr1);
 }
+void store(int n)
+{
+     int k,a,b;
+    printf("total numbers =\n");
+    scanf("%d",&n);
+    FILE* pr;
+    pr=fopen("storeR2F.txt","w");
+    printf("enter the range of number a,b(a must be less than b)\n");
+    scanf("%d%d",&a,&b);
+    char x[10]={' '};
+    for(int i=0;i<n;i++)
+    {
+        k=rand()%(a-b+1)+a;
+        printf("%d ",k);
+        fprintf(pr,"%d",k);
+        fprintf(pr,"%s",x);
+    }
+    fclose(pr);
+}
+void store_to_arry(int n,int arr1[100])
+{
+    FILE*pr2;
+    pr2=fopen("storeR2F.txt","r");
+    for(int i=0;i<n;i++)
+    {
+        fscanf(pr2,"%d",&arr1[i]);
+        printf(" %d",arr1[i]);
+    }
+    fclose(pr2);
+}
+void sort(int n,int arr1[100])
+{
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr1[i]>arr1[j])
+            {
+                temp=arr1[j];
+                arr1[j]=arr1[i];
+                arr1[i]=temp;
+            }
+        }
+    }
+    FILE*pr3;
+    pr3=fopen("storeS2F.txt","w");
+    for(int i=0;i<n;i++)
+    {
+        fprintf(pr3,"%d",&arr1[i]);
+        printf(" %d",arr1[i]);
+    }
+    fclose(pr3);
+}
+
